066c19fe2ebf5fc7e1e528e5c078159dafdfa4d8
Maky <franklinjaradev@gmail.com>
2021-05-19 14:30:47 -0400

    Added new date :)

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const hoursEl = document.getElementById("hours");
 const minsEl = document.getElementById("mins");
 const secondsEl = document.getElementById("seconds");
 
-const airBnB = "14 May 2021";
+const airBnB = "12 June 2021";
 
 function countdown() {
   const sydneyDate = new Date(airBnB);
